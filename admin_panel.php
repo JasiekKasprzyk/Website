81c4c531d9eb05d1a2bc8a9b43c5908fa4dd5888
MisterCodePL <mistercode11@gmail.com>
2016-05-24 21:05:11 +0200

    Added index.php, admin_panel.php, style.css 24/05/2016

---
+++ admin_panel.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTMP>
+<html lang="pl">
+
+<head>
+	<meta charset="utf-8" />
+	<title>Panel administratora</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chroma=1" />
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+</head>
+
+
+<body>
+	<div class="container-fluid">
+		<div class="row space"></div>
+		<div class="row">
+			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 space"></div>
+			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 space">
+			
+			text
+			
+			</div>
+			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 space"></div>
+		</div>
+		<div class="row space"></div>
+	</div>
+</body>
+
+</html>
