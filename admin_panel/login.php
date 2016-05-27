64de2cf2387e3a5f9f271dc2e6fe9d580b6ac96b
MisterCodePL <mistercode11@gmail.com>
2016-05-27 14:58:10 +0200

    hashed passwords

--- admin_panel/login.php
+++ admin_panel/login.php
@@ -19,25 +19,33 @@
 		$password=$_POST['password'];
 		
 		$login=htmlentities($login, ENT_QUOTES, "UTF-8");
-		$password=htmlentities($password, ENT_QUOTES, "UTF-8");
 		
 		
 		if($result = $connection->query(
-		sprintf("SELECT * FROM administrators WHERE login='%s' AND password='%s'", 
-		mysqli_real_escape_string($connection, $login),
-		mysqli_real_escape_string($connection, $password))))
+		sprintf("SELECT * FROM administrators WHERE login='%s'", 
+		mysqli_real_escape_string($connection, $login))))
 		{
 			$how_many_users = $result->num_rows;
 			if($how_many_users>0)
 			{
-				$_SESSION['islogged']=true;
 				$line=$result->fetch_assoc();
-				$_SESSION['id']=$line['id'];
-				$_SESSION['username']=$line['username'];
 				
-				unset($_SESSION['error']);
-				$result->close();
-				header('Location: session.php');
+				if(password_verify($password, $line['password']))
+				{
+					$_SESSION['islogged']=true;
+					$_SESSION['id']=$line['id'];
+					$_SESSION['username']=$line['username'];
+					
+					unset($_SESSION['error']);
+					$result->close();
+					header('Location: session.php');
+				}
+				else
+				{
+					$_SESSION['error']='<span style="color:red">Nieprawidłowe hasło</span>';
+					header('Location: index.php');
+					
+				}
 			}
 			else
 			{
---
+++ admin_panel/secret_file.php
@@ -0,0 +1,22 @@
+<?php
+	if(isset($_POST['password'])) 
+	{
+		$password = $_POST['password'];
+		$hash_password=password_hash($password, PASSWORD_DEFAULT);
+		echo $hash_password;
+	}
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		<form method="post">
+		<input type="password" name="password"/>
+		<input type="submit" />
+		</form>
+	</body>
+</html>
