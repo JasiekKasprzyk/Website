92f6d8a8eaf2830b812555b908e1c44be56a6fed
MisterCodePL <mistercode11@gmail.com>
2016-06-24 16:58:53 +0200

    restyled admin_panel/index.php

--- admin_panel/index.php
+++ admin_panel/index.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	
-	if(isset($_SESSION['islogged']) && $_SESSION['islogged']==true)
+	if(isset($_SESSION['islogged']))
 	{
 		header('Location: session.php');
 		exit();
@@ -13,36 +13,29 @@
 
 <head>
 	<meta charset="utf-8" />
-	<title>Panel administratora</title>
+	<title>Onlykom - panel administratora</title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chroma=1" />
-	<link rel="stylesheet" href="../style.css" type="text/css" />
+	<link rel="stylesheet" href="style.css" type="text/css" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<link href='https://fonts.googleapis.com/css?family=Lato:400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 </head>
 
 
 <body>
-	<div class="container-fluid">
-		<div class="row space"></div>
-		<div class="row">
-			<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 space"></div>
-			<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 login-space login-background">
-				<div class="login">
-					<form action="login.php" method="post">
-					Zaloguj się do panelu administratora:<br/>
-					Login:<br/>
-					<input type="text" name="login"/><br />
-					Hasło: <br/>
-					<input type="password" name="password"/><br /><br/>
-					<input type="submit" value="Zaloguj się!" />
-					</form>
+	<div class="container">
+		<div class="login-interface">
+			<form action="login.php" method="post">
+				Zaloguj się do panelu admina:<br/>
+				Login:<br/>
+				<input type="text" name="login" class="text"/><br />
+				Hasło: <br/>
+				<input type="password" name="password" class="text"/><br />
+				<input type="submit" value="Zaloguj się!" class="submit" />
+			</form>
 <?php
 	if(isset($_SESSION['error'])) echo $_SESSION['error'];
 ?>
-				</div>
-			</div>
 		</div>
-		<div class="row space"></div>
 	</div>
 </body>
 
