314d915d103acbe9e89a845d943d89d329007348
MisterCodePL <mistercode11@gmail.com>
2016-08-13 19:47:26 +0200

    Stylized inputs in Index and Upload.

--- views/Admin_panel/Upload.php
+++ views/Admin_panel/Upload.php
@@ -21,9 +21,21 @@
 			<div class="add-photo-menu">
 				<h1>Dodaj zdjęcie!</h1>
 				<form method="POST" ENCTYPE="multipart/form-data">
-					<input type="file" name="fileToUpload" id="fileToUpload" class="upload"/><br />
+					<div class="whole-input">
+						<input id="uploadFile" placeholder="Wybierz plik" disabled="disabled" />
+						<div class="fileUpload">
+							<span>Wybierz plik</span>
+							<input type="file" name="fileToUpload" id="fileToUpload" class="upload"/><br />
+						</div>
+						<div style="clear: both"></div>
+					</div>
 					<input type="submit" value="Wyślij!" name="submit" class="upload"/><br />
 				</form>
+				<script type="text/javascript">
+					document.getElementById("fileToUpload").onchange = function () {
+					    document.getElementById("uploadFile").value = this.value;
+					};
+				</script>
 <?php
 	if(isset($_SESSION['errorF']))
 	{
